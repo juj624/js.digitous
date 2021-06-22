5532835542099ce750eac8feb49acd79af7b52c0
juj624 <julienpenot@outlook.com>
2021-06-22 16:46:51 +0200

    jours-5.js

--- jours-5.js
+++ jours-5.js
@@ -1,20 +1,35 @@
+// const { resourceLimits } = require("worker_threads");
 
-//1//
-function calculate(number1, operateur, number2) {
-    if (operateur === "+"){
-        console.log(number1 + number2);
-    }if (operateur === "-") {
-        console.log(number1 - number2);
-    }if (operateur === "/") {
-        console.log(number1 / number2);
-    }if ( operateur === "%") {
-        console.log(number1 % number2);
-    }
+// //1//
+// function calculate(number1, operateur, number2) {
+//     if (operateur === "+"){
+//         console.log(number1 + number2);
+//     }if (operateur === "-") {
+//         console.log(number1 - number2);
+//     }if (operateur === "/") {
+//         console.log(number1 / number2);
+//     }if ( operateur === "%") {
+//         console.log(number1 % number2);
+//     }
+// }
+// //calculate(4,"+",2);
+// console.log(process.argv);
+
+// // calculate(parseInt(process.argv[2]),process.argv[3],parseInt(process.argv[4]));
+
+// 2//
+ 
+
+// // var {addition,multiply} = require("./table-untils");
+
+// // addition(parseInt(process.argv[2]));
+
+// // multiply(parseInt(process.argv[2]));
+
+//04
+
+
+
+function play() {
+    var mysteryNum = Math.floor(Math.random() * (1-100 + 1)) + max;
 }
-calculate(4,"+//",2);
-console.log(process.argv);
-
-calculate(parseInt(process.argv[2]),process.argv[3],parseInt(process.argv[4]));
-
-//2//
-
---
+++ table-untils.js
@@ -0,0 +1,19 @@
+
+//3
+
+function addition(numberEntier2) {
+    for (var i = 1; i<=10; i++) {
+        console.log(numberEntier2 + i);
+    }
+}
+function multiply(numberEntier) {
+    for (var i = 1; i<=10; i++) {
+        console.log(numberEntier * i);
+    } 
+        
+}
+
+ module.exports = {
+    addition,
+    multiply,
+ }
